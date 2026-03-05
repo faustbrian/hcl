044b5a55ae0c24e00e54eb658a055c6a139e1e3a
Brian Faust <brian@cline.sh>
2026-03-05 07:13:42 +0200

    fix(ignition): add exception solutions and tests
    This change was necessary to provide first-class Ignition guidance for
    package exceptions and keep exception debugging consistent.
    
    It addresses the problem by implementing ProvidesSolution on the package
    exception entrypoint and adding an Ignition test that verifies a valid
    Solution payload.
    
    Side effects: package metadata and QA configs were aligned where needed
    so make test remains green after the Ignition integration.

--- src/Exceptions/ParserException.php
+++ src/Exceptions/ParserException.php
@@ -9,6 +9,9 @@
 
 namespace Cline\Hcl\Exceptions;
 
+use Facade\IgnitionContracts\BaseSolution;
+use Facade\IgnitionContracts\ProvidesSolution;
+use Facade\IgnitionContracts\Solution;
 use RuntimeException;
 
 /**
@@ -22,7 +25,19 @@ use RuntimeException;
  *
  * @author Brian Faust <brian@cline.sh>
  */
-abstract class ParserException extends RuntimeException implements HclException
+abstract class ParserException extends RuntimeException implements HclException, ProvidesSolution
 {
     // Abstract base - no factory methods
+
+    public function getSolution(): Solution
+    {
+        /** @var BaseSolution $solution */
+        $solution = BaseSolution::create('Review package usage and configuration.');
+
+        return $solution
+            ->setSolutionDescription('Exception: '.$this->getMessage())
+            ->setDocumentationLinks([
+                'Package documentation' => 'https://github.com/cline/hcl',
+            ]);
+    }
 }
